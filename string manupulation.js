be787c4936488a2c591bc59aba27c2244ae35716
bhabesh2 <jsarma058@gmail.com>
2023-03-28 20:34:18 +0530

    string manupulation

---
+++ string manupulation.js
@@ -0,0 +1,18 @@
+
+
+function manipulateString(str) {
+  const manipulatedStr = str.toUpperCase();
+
+  function logString() {
+    console.log(`The manipulated string is: ${manipulatedStr}`);
+  }
+
+  return logString;
+}
+
+function logString() {
+  console.log(`The manipulated string is: ${manipulatedStr}`);
+}
+
+manipulateString("hello, world!")(logString);
+// Output: The manipulated string is: HELLO, WORLD!
